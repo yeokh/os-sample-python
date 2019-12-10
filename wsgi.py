4038e9139971d4d008c6fe227d18acfaebab9b6c
yeokh <yeokh@yahoo.com>
2019-12-10 12:26:16 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,14 @@
 from flask import Flask
-# import requests
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-  return "Hello From Python Demo with Red Hat OpenShift!"
+    return "Hello From Python Demo with Red Hat OpenShift!"
 
 @app.route("/test")
 def test():
-  return "Testing"
-
-@app.route("/geturldata")
-def geturldata():
-  # url_req = requests.get("https://data.gov.sg/api/action/datastore_search?resource_id=f9dbfc75-a2dc-42af-9f50-425e4107ae84&limit=10")
-  # json_data = url_req.json()
-  json_data = "String variable"
-  return json_data
+    return "Testing"
 
 if __name__ == "__main__":
     application.run()
